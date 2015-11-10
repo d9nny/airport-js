619630d1b6387b62349ba0b24de9c91289e4ceaf
danwheeler <da-wheeler@hotmail.co.uk>
2015-11-10 17:08:51 +0000

    throw erorrs added

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -29,32 +29,50 @@ describe("Airport", function() {
     airport.land(plane);
     expect(plane.land).toHaveBeenCalled();
   });
+
   it("A landed plane should be in the hangar", function() {
+    spyOn(Math, 'random').andReturn(0.9);
     airport.land(plane);
     expect(airport.isInAirport(plane)).toEqual(true);
   });
+
+  it("Should not allow a plane to land when it is stormy", function() {
+    spyOn(Math, 'random').andReturn(0.0);
+    expect(function(){airport.land(plane);}).toThrow(Error("Too stormy to land"));
+  });
+
   it('Should not allow a plane to land when the airport is full', function() {
+    spyOn(Math, 'random').andReturn(0.9);
     for (var i = 0; i < capacity; i++) {
        airport.land(plane);
      }
      expect(function(){airport.land(plane);}).toThrow(Error('Airport is full'));
   });
 
-  it("Should return tell us when the weather", function() {
+  it("Should tell us when the weather", function() {
     expect(airport.weather()).toBeTruthy();
   });
-  it("Should return tell us when the weather is stormy", function() {
+
+  it("Should tell us when the weather is stormy", function() {
     spyOn(Math, 'random').andReturn(0.0);
     expect(airport.isStormy()).toBe(true);
   });
+
   it("should't allow a plane to take off when it's stormy", function() {
     spyOn(Math, 'random').andReturn(0.0);
     expect(function(){airport.takeOff(plane);}).toThrow(Error("Can't take off. Stormy"));
   });
+
   it("should allow a plane to take off when it's sunny", function() {
     spyOn(Math, 'random').andReturn(0.9);
+    airport.land(plane);
     airport.takeOff(plane);
-    expect(airplane.hasPlane(plane)).toBe(false);
+    expect(airport.isInAirport(plane)).toBe(false);
+  });
+
+  it("should not allow a plane to take off if its not in the airport", function () {
+    spyOn(Math, 'random').andReturn(0.9);
+    expect(function(){airport.takeOff(plane);}).toThrow(Error('Not in airport'));
   });
 
 });
